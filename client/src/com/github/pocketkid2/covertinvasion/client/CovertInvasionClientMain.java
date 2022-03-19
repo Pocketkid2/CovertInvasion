b7bc62103f931b6d14222ea2f0facc8875862282
Adam Taylor <afranklint16@gmail.com>
2022-03-19 12:54:06 -0600

    Created initial client and server projects

---
+++ client/src/com/github/pocketkid2/covertinvasion/client/CovertInvasionClientMain.java
@@ -0,0 +1,13 @@
+package com.github.pocketkid2.covertinvasion.client;
+
+public class CovertInvasionClientMain {
+
+	public CovertInvasionClientMain() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ server/src/com/github/pocketkid2/covertinvasion/server/CovertInvasionServerMain.java
@@ -0,0 +1,13 @@
+package com.github.pocketkid2.covertinvasion.server;
+
+public class CovertInvasionServerMain {
+
+	public CovertInvasionServerMain() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
